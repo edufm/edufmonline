0af9def0615b54b6e981d7f2b6491bc52c87efd3
Eduardo Ferrari <eduferrarimagalhaes@hotmail.com>
2021-07-10 03:23:10 -0300

    [FEAT] Added language selection option between PT and ENG

---
+++ public/ing/tipuesearch/tipuesearch_content.js
@@ -0,0 +1,101 @@
+
+var tipuesearch = {
+    "pages": [
+
+        {
+            "title": "Home", "text": "Home",
+            "tags": "Eduardo Home Index",
+            "url": "./Home.html"
+        },
+        {
+            "title": "Resume",
+            "text": "Meu curriculo completo",
+            "tags": "Curriculo Experiencia Faculdade Professional Job Work",
+            "url": "./Resume.HTML"
+        },
+        {
+            "title": "Professional Experiences",
+            "text": "Historico Curriculo Professional",
+            "tags": "Pessoal Professional Independente Projetos",
+            "url": "./Prof.HTML"
+        },
+        {
+            "title": "Projetos Academicos",
+            "text": "Projetos realizados dentro da grade curricular",
+            "tags": "Programming Python HTML Mecanica Mecatronica Insper Projetos",
+            "url": "./Indenp.HTML"
+        },
+
+
+        {
+            "title": "TextTagger",
+            "text": "A python module used to analyze texts according to their characteristics and tags",
+            "tags": "NLP Natural Language Processing Text Programming Python",
+            "url": "./Indenp/TextTagger.HTML"
+        },
+        {
+            "title": "SimpleWiki",
+            "text": "Development of a scraper and simplifier for wikipedia definition",
+            "tags": "NLP Natural Language Processing Text Programming Python",
+            "url": "./Indenp/SimpleWiki.HTML"
+        },
+        {
+            "title": "Analise de Sentimento",
+            "text": "Use Tweets to analyze sentiment and correlate news with Ibovespa returns",
+            "tags": "NLP Natural Language Processing Text Programming Python Market Financial",
+            "url": "./Indenp/Sentimento.HTML"
+        },
+        {
+            "title": "RobotSim",
+            "text": "A Python Module designed for robot simulation and animation",
+            "tags": "Robotics Industry Mechanical Programming Python",
+            "url": "./Indenp/RobotSim.HTML"
+        },
+        {
+            "title": "Website",
+            "text": "Development of a website that students can use as their first curriculum",
+            "tags": "Programming WebSite FrontEnd",
+            "url": "./Indenp/Website.HTML"
+        },
+        {
+            "title": "Game",
+            "text": "Prototype of a turn-based arcade game with online rank",
+            "tags": "Programming Game Arcade Rank",
+            "url": "./Indenp/Game.HTML"
+        },
+
+
+        {
+            "title": "Pandhora Investimentos - Quant Researcher",
+            "text": "Associate at Pandhora Investimentos with a focus on market modeling, portfolio allocation, risk monitoring and software development",
+            "tags": "Professional Job Work Pandhora Market Financial Programming Research",
+            "url": "./Prof/Pandhora.HTML"
+        },
+        {
+            "title": "Iniciação Cientifica - High Frquency Data",
+            "text": "Analysis of stock behavior and creation of investment algorithms using high frequency data (tick-by-tick)",
+            "tags": "Professional Job Work CNPQ Sientific Academic Market Financial Programming Research",
+            "url": "./Prof/ICHighFrequency.HTML"
+        },
+        {
+            "title": "Metricis - Cloud Computing",
+            "text": "Analysis of Cloud Implementation Projects for the Federal Government",
+            "tags": "Professional Job Work Metricis Cloud Programming Research",
+            "url": "./Prof/CloudComputingMetricis.HTML"
+        },
+        {
+            "title": "MIT: Global Teaching Lab",
+            "text": "Creating and performing a workshop with high school students to bring them closer to programming and technology concepts",
+            "tags": "Professional Job Work Volunteer MIT Insper WorkShop",
+            "url": "./Prof/MITTeachingLab.HTML"
+        },
+
+
+        {
+            "title": "Insper",
+            "text": "Insper WebSite",
+            "tags": "Insper Graduation Academic",
+            "url": "http://www.insper.edu.br/"
+        },
+    ]
+};
